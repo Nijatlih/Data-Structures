cd4edaa296dab2968af42e6ed792a40afeef2d01
nijat <nijat.de@gmail.com>
2022-08-26 13:44:45 +0200

    Assignment 5 SunsetViews

--- DSA-03-Stacks/src/main/java/SunsetViews.java
+++ DSA-03-Stacks/src/main/java/SunsetViews.java
@@ -1,14 +1,33 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
 public class SunsetViews {
     public static void main(String[] args) {
         int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
         sunsetViews(buildings,"EAST").forEach(System.out::println);
+        sunsetViews(buildings,"WEST").forEach(System.out::println);
     }
     public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
-        // Write your code here.
-        // Write your code here.
-        // Write your code here.
-        return new ArrayList<Integer>();
+
+        Stack<Integer> candidateBuildings=new Stack<>(); // O(n) space complexity
+        int startIdx=0;
+        int step=1;
+        // direction.equals("EAST")
+        if(direction.equals("WEST")){
+            startIdx= buildings.length-1; step=-1;}
+        int idx=startIdx;
+        while (idx>=0 && idx< buildings.length){
+            while(!candidateBuildings.isEmpty()&& buildings[candidateBuildings.peek()]<=buildings[idx]){
+                candidateBuildings.pop();
+            }
+            candidateBuildings.push(idx);
+            idx+=step;
+        }
+        //O(n)
+        if (direction.equals("WEST"))
+            Collections.reverse(candidateBuildings); //O(n)
+        return new ArrayList<Integer>(candidateBuildings); // O(2n)= O(n) time complexity
     }
+
 }
